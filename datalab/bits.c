195aa33bbbaa90ba8e06148145ba1c7394f66ddd
Louis Deng <louisaeilotd@gmail.com>
2025-10-11 20:49:30 +0800

    Add bitXOR and tmin

--- datalab/bits.c
+++ datalab/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * Louis Aeilot
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return ~((~(x&~y))&(~((~x)&y)));
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +152,7 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  return 1<<31;
 }
 //2
 /*
@@ -165,6 +163,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
+  
   return 2;
 }
 /* 
